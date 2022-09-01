ed5f03b7041d37776a7ce4c4661c3d6a33324b04
Georgy Ruzavin <ruzavin02@mail.ru>
2022-09-01 09:21:46 +0300

    MS-1: Initialize project

---
+++ src/main/java/ru/ruzavin/rmrproshivkamessenger/RmrProshivkaMessengerApplication.java
@@ -0,0 +1,13 @@
+package ru.ruzavin.rmrproshivkamessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmrProshivkaMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmrProshivkaMessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ruzavin/rmrproshivkamessenger/RmrProshivkaMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ruzavin.rmrproshivkamessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmrProshivkaMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
